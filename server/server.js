f1cf0d5fa0b5dd6786d3b097eee17f7b06b2a89a
Howard Yang <howardyang25@gmail.com>
2019-08-29 15:02:43 -0700

    Gallery component renders through proxy server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const port = 3000;
+const app = express();
+
+app.use('/restaurants/:rid', express.static(path.resolve(__dirname, '../public')));
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
