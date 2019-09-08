eeb9cef12d3eb267280c67eec6e6762cded8de14
Howard Yang <howardyang25@gmail.com>
2019-09-07 19:53:25 -0700

    Implemented fonts and integrated other components

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
+const compression = require('compression');
 const port = process.env.PORT || 3000;
 const app = express();
-
+app.use(compression());
 app.use('/restaurants/:rid', express.static(path.resolve(__dirname, '../public')));
 
 app.listen(port, () => {
